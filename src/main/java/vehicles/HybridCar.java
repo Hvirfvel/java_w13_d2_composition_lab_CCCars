df44229f1a119729342924a985e0df6f1d348bfa
hvirfvel <carolinehvirfvel@hotmail.com>
2022-05-09 20:50:42 +0100

    added tests for hybrid car and methods to pass

--- src/main/java/vehicles/HybridCar.java
+++ src/main/java/vehicles/HybridCar.java
@@ -8,9 +8,17 @@ public class HybridCar extends Vehicle{
     Engine engine;
     Battery battery;
 
-    public HybridCar(int price, String colour) {
+    public HybridCar(int price, String colour, Engine engine, Battery battery) {
         super(price, colour);
         this.engine = engine;
         this.battery = battery;
+    }
+
+    public int getEngineCylinders() {
+        return engine.getCylinders();
+    }
+
+    public int getBatteryLifespan() {
+        return battery.getLifespan();
     }
 }
--- src/test/java/HybridCarTest.java
+++ src/test/java/HybridCarTest.java
@@ -1,2 +1,41 @@
+import components.Battery;
+import components.Engine;
+import org.junit.Before;
+import org.junit.Test;
+import vehicles.HybridCar;
+
+import static org.junit.Assert.assertEquals;
+
 public class HybridCarTest {
+
+    Engine engine;
+    Battery battery;
+    HybridCar hybridCar;
+
+    @Before
+    public void beforeEach() {
+        engine = new Engine(6);
+        battery = new Battery(8);
+        hybridCar = new HybridCar(40000, "Black", engine, battery);
+    }
+
+    @Test
+    public void getPrice() {
+        assertEquals(40000, hybridCar.getPrice());
+    }
+
+    @Test
+    public void getColour() {
+        assertEquals("Black", hybridCar.getColour());
+    }
+
+    @Test
+    public void getEngineCylinders() {
+        assertEquals(6, hybridCar.getEngineCylinders());
+    }
+
+    @Test
+    public void getBatteryLifespan() {
+        assertEquals(8, hybridCar.getBatteryLifespan());
+    }
 }
