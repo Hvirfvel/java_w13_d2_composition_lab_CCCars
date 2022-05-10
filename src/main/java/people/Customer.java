31b2bc39dc992724eb1f71bd35833716449697fc
hvirfvel <carolinehvirfvel@hotmail.com>
2022-05-10 11:24:46 +0100

    added tests for customer and methods to pass

--- src/main/java/people/Customer.java
+++ src/main/java/people/Customer.java
@@ -1,5 +1,6 @@
 package people;
 
+import vehicles.Car;
 import vehicles.Vehicle;
 
 import java.util.ArrayList;
@@ -12,5 +13,22 @@ public class Customer {
     public Customer(int money) {
         this.money = money;
         vehicles = new ArrayList<Vehicle>();
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getVehicleCount() {
+        return vehicles.size();
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+    public void buyVehicle(Vehicle vehicle) {
+        addVehicle(vehicle);
+        money -= vehicle.getPrice();
     }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,43 @@
+import components.Engine;
+import org.junit.Before;
+import org.junit.Test;
+import people.Customer;
+import vehicles.Car;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    Customer customer;
+    Car car;
+    Engine engine;
+
+    @Before
+    public void beforeEach() {
+        customer = new Customer(50000);
+        car = new Car(32000, "Blue", engine);
+    }
+
+    @Test
+    public void canGetMoney() {
+        assertEquals(50000, customer.getMoney());
+    }
+
+    @Test
+    public void canGetVehicleCount() {
+        assertEquals(0, customer.getVehicleCount());
+    }
+
+    @Test
+    public void canAddCar() {
+        customer.addVehicle(car);
+        assertEquals(1, customer.getVehicleCount());
+    }
+
+    @Test
+    public void canBuyCar() {
+        customer.buyVehicle(car);
+        assertEquals(1, customer.getVehicleCount());
+        assertEquals(18000, customer.getMoney());
+    }
 }
